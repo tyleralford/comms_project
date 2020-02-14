5e124a8743e022760cc36118a853738d7986cb30
Patrick <patrickdberry802@gmail.com>
2020-02-14 13:56:10 -0600

    Fixing code to plot better noise floor (within band of interest) & updated code to plot new data.

--- data/plotData.m
+++ data/plotData.m
@@ -11,19 +11,19 @@ function plotData(filename,H)
   X = load(filename);
   FFT = @(X,varargin) 20*log10(abs(fftshift(fft(X,varargin{:}))));
   Xf = FFT(X,[],2);
-  noise_floor = mean(Xf,'all');
   f = fs/2*linspace(-1,1,size(Xf,2));
 
   lgc = (500<=f & f<4.5e3);
   f0 = f(lgc);
   Xf0 = Xf(:,lgc);
+  noise_floor = mean(Xf0,'all');
   
   hold('on');
   h=plot(f0/1e3,Xf0);
   arrayfun(@(h)set(h.Annotation.LegendInformation,'IcOnDiSpLaYsTyLe','oFf'),h);
   set(h(end).Annotation.LegendInformation,'IcOnDiSpLaYsTyLe','oN');
   h=plot(f0/1e3,Xf0);
-  h= plot([min(f0),max(f0)]/1e3,noise_floor+[0,0],'k--','LineWidtH',2);
+  h=plot([min(f0),max(f0)]/1e3,noise_floor+[0,0],'k--','LineWidtH',2);
   grid('on');
   h=legend({'FFT of Time Series','Noise Floor'},'Location','sOuThEaSt');h.FontSize=16;
   h=xlabel('\bffrequncy (kHz)');set(h,'FontSizE',16);
