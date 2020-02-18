1bad628c07bd40e2cc0ad944ef8d5675811c7bac
tyleralford <alford.tyler94@gmail.com>
2020-02-18 17:02:53 -0600

    Update main_test.py
    a couple more fixes. this code should work

--- main_test.py
+++ main_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from libpruio import *
+#from libpruio import *
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, fftfreq
+from scipy.fftpack import fft
 import time
 import peakutils
-from peakutils.plot import plot as pplot
 from scipy import signal
 
 print
@@ -33,31 +32,182 @@ print("Number of data sets = %s" % rtotal)
 print
 time.sleep(0.5)
 
-io = pruio_new(PRUIO_DEF_ACTIVE, 0, 0, 0)					#create ctypes pointer to the pruio structure
+
+def sample():
+    """
+    IO = io.contents					#pointer dereferencing, using contents member
+    if IO.Errr:
+        raise AssertionError("pruio_new failed (%s)" %IO.Errr)
+
+    raw_input("Press Enter when ready to start sampling.")
+    print
+    print('Sampling now...\n')
+
+    if pruio_config(io, tSamp, activeADC, SampRat, 0):			#upload settings, start IO mode, 12-bit resolution
+        raise AssertionError("config failed (%s)" % IO.Errr)
+
+    if pruio_rb_start(io):
+        raise AssertionError("rb_start failed (%s)" % IO.Errr)
+
+    data_array = []
+    data = []
+    p = 0
+    t0 = time.time()
+    while p < rtotal:
+        pruio_config(io, tSamp, activeADC, SampRat, 0)
+        pruio_rb_start(io)
+        p0 = IO.Adc.contents.Value						#pointer to ADC value (start of ring buffer)
+        while IO.DRam[0] > tSamp: time.sleep(0.001)				#wait for sampling to finish
+        i = 0
+        result = 0
+	#pull data from buffer and move to list
+        data_array = []
+        while i < tSamp:
+            result = p0[i]
+            data_array.append(result)
+            i += 1
+        data.append(data_array)							#place completed data run in master list
+        p += 1
+
+    t1 = time.time()
+    tim = t1 - t0
+    print("Data collected in %s seconds." % tim)
+    print
+    return data
+    """
+
+def windowing(data):
+#apply windowing
+    t0 = time.time()
+    p = 0
+    data0 = data
+    while p < rtotal:
+        data0[p] = data0[p] * signal.kaiser(tSamp, 0.8, sym=False)
+        p += 1
+    t1 = time.time()
+    tim = t1 - t0
+    print("Windowing completed in %s seconds" % tim)
+    print
+    return data0
+
+def fft0(data0):
+#compute FFT
+    p = 0
+    fft_data0 = []
+    t0 = time.time()
+    while p < rtotal:
+        fft_data0.append(np.abs(fft(data0[p])))
+        p += 1
+    t1 = time.time()
+    tim = t1 - t0
+    print("FFT completed in %s seconds" % tim)
+    print
+    return fft_data0
+
+def ave_fft0(fft_data0):
+#average all data sets
+    ave_fft0 = np.zeros(tSamp)
+    p = 0
+    t0 = time.time()
+    while p < rtotal:
+        ave_fft0 = ave_fft0 + fft_data0[p]
+        p += 1
+    ave_fft0 = ave_fft0 / rtotal
+    t1 = time.time()
+    tim = t1 - t0
+    print("Averaging completed in %s seconds" % tim)
+    print
+    return ave_fft0
+
+def peak_find(ave_fft0):
+    #Peak finding stuff
+    threshold = 2
+    peaks_x = []
+    print("Peak finding. First pass.")
+    print
+    while len(peaks_x) < 8:
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+        threshold += -0.1
+
+    threshold += -0.1
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Second pass.")
+    print
+    while len(peaks_x) > 12:
+        threshold += 0.01
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+
+    threshold += -0.02
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Third pass.")
+    print
+    while len(peaks_x) >= 8:
+        threshold += 0.001
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+
+    threshold += -0.002
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Fourth pass.")
+    print
+    while len(peaks_x) > 6:
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+        threshold += 0.0001
+
+    threshold += -0.0002
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Fifth pass.")
+    print
+    while len(peaks_x) > 6:
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+        threshold += 0.00001
+
+    threshold += -0.00002
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Sixth pass.")
+    print
+    while len(peaks_x) > 6:
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+        threshold += 0.000001
+
+    threshold += -0.000002
+    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+    print("Peak finding. Last pass.")
+    print
+    while len(peaks_x) > 6:
+        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
+        threshold += 0.0000001
+    #print("Threshold = %s" % threshold)
+    #print("Number of peaks = %s" % len(peaks_x))
+    return peaks_x
+
+
+
+
+#io = pruio_new(PRUIO_DEF_ACTIVE, 0, 0, 0)					#create ctypes pointer to the pruio structure
 try:
-    do_cal = raw_input('Do you want to use cal data? (y/n)')
+    do_cal = raw_input('Do you want to use cal data? (y/n)\n')
     if do_cal == 'y':
-        cal = raw_input('Would you like to gather cal data? (y/n)')
+        cal = raw_input('Would you like to gather cal data? (y/n)\n')
         if cal == 'y':
             cal_data = sample()
             cal0 = windowing(cal_data)
-            cal_fft = fft(cal0)
-            cal_ave = average_fft(cal_fft)
+            cal_fft = fft0(cal0)
+            cal_ave = ave_fft0(cal_fft)
             np.savetxt('cal_data.txt', cal_ave)
         else:
             cal_ave = np.loadtxt('cal_data.txt')
     else:
         cal_ave = 0
 
-    data = sample()
+    #data = sample()
 
-    #data = np.loadtxt('data/data_-40db_snr.txt', dtype='float32')
+    data = np.loadtxt('data/data_-10db_snr.txt', dtype='float32')
 
     data0 = windowing(data)
 
-    fft_data0 = fft(data0)
+    fft_data0 = fft0(data0)
 
-    ave_fft0 = average_fft(fft_data0) - cal_ave
+    ave_fft0 = ave_fft0(fft_data0) - cal_ave
 
 
     #generate frequency vector
@@ -118,139 +268,5 @@ try:
     plt.show()
 
 finally:
-    pruio_destroy(io)							#end it
+    #pruio_destroy(io)							#end it
     print('Done')
-
-
-def sample():
-    IO = io.contents					#pointer dereferencing, using contents member
-    if IO.Errr:
-        raise AssertionError("pruio_new failed (%s)" %IO.Errr)
-
-    raw_input("Press Enter when ready to start sampling.")
-    print
-    print('Sampling now...\n')
-
-    if pruio_config(io, tSamp, activeADC, SampRat, 0):			#upload settings, start IO mode, 12-bit resolution
-        raise AssertionError("config failed (%s)" % IO.Errr)
-
-    if pruio_rb_start(io):
-        raise AssertionError("rb_start failed (%s)" % IO.Errr)
-
-    data_array = []
-    data = []
-    p = 0
-    t0 = time.time()
-    while p < rtotal:
-        pruio_config(io, tSamp, activeADC, SampRat, 0)
-        pruio_rb_start(io)
-        p0 = IO.Adc.contents.Value						#pointer to ADC value (start of ring buffer)
-        while IO.DRam[0] > tSamp: time.sleep(0.001)				#wait for sampling to finish
-        i = 0
-        result = 0
-	#pull data from buffer and move to list
-        data_array = []
-        while i < tSamp:
-            result = p0[i]
-            data_array.append(result)
-            i += 1
-        data.append(data_array)							#place completed data run in master list
-        p += 1
-
-    t1 = time.time()
-    tim = t1 - t0
-    print("Data collected in %s seconds." % tim)
-    print
-    return data
-
-def windowing(data):
-#apply windowing
-    t0 = time.time()
-    p = 0
-    data0 = data
-    data1 = data
-    while p < rtotal:
-        data0[p] = data0[p] * signal.kaiser(tSamp, 0.8, sym=False)
-        p += 1
-    t1 = time.time()
-    tim = t1 - t0
-    print("Windowing completed in %s seconds" % tim)
-    print
-    return data0
-
-def fft(data0):
-#compute FFT
-    p = 0
-    fft_data0 = []
-    t0 = time.time()
-    while p < rtotal:
-        fft_data0.append(np.abs(fft(data0[p])))
-        p += 1
-    t1 = time.time()
-    tim = t1 - t0
-    print("FFT completed in %s seconds" % tim)
-    print
-    return fft_data0
-
-def ave_fft0(fft_data0):
-#average all data sets
-    ave_fft0 = np.zeros(tSamp)
-    p = 0
-    t0 = time.time()
-    while p < rtotal:
-        ave_fft0 = ave_fft0 + fft_data0[p]
-        p += 1
-    ave_fft0 = ave_fft0 / rtotal
-    t1 = time.time()
-    tim = t1 - t0
-    print("Averaging completed in %s seconds" % tim)
-    print
-    return ave_fft0
-
-def peak_find(ave_fft0):
-#Peak finding stuff
-    peaks_x = []
-    print("Peak finding. First pass.")
-    print
-    while len(peaks_x) < 8:
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-        threshold += -0.1
-
-    print("Peak finding. Second pass.")
-    print
-    while len(peaks_x) > 12:
-        threshold += 0.01
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-
-    print("Peak finding. Third pass.")
-    print
-    while len(peaks_x) >= 8:
-        threshold += 0.001
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-
-    threshold += -0.001
-    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-    print("Peak finding. Fourth pass.")
-    print
-    while len(peaks_x) > 6:
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-        threshold += 0.0001
-
-    threshold += -0.0002
-    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-    print("Peak finding. Fifth pass.")
-    print
-    while len(peaks_x) > 6:
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-        threshold += 0.00001
-
-    threshold += -0.00002
-    peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-    print("Peak finding. Last pass.")
-    print
-    while len(peaks_x) > 6:
-        peaks_x = peakutils.indexes(ave_fft0,thres=threshold,min_dist=peak_shift)
-        threshold += 0.000001
-    #print("Threshold = %s" % threshold)
-    #print("Number of peaks = %s" % len(peaks_x))
-    return peaks_x
